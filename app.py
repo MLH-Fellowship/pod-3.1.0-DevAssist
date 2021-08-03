696556dc7870a48232ec17bf74d6da351cc15d69
simran1199 <simran3579singh@gmail.com>
2021-08-04 02:21:57 +0530

    group chat room added

--- app.py
+++ app.py
@@ -1,18 +1,23 @@
 from flask import Flask, render_template, redirect, url_for, send_file, request
 from imports.ErrAutoSearch import main_func
 from werkzeug.utils import secure_filename
+from flask_socketio import SocketIO, emit
 import os
 
 UPLOAD_FOLDER = os.path.dirname(os.path.abspath(__file__))
 ALLOWED_EXTENSIONS = {'py'}
 
 app = Flask(__name__)
-
+socketio = SocketIO(app, cors_allowed_origins="*")
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 
 
 def allowed_file(filename):
     return '.' in filename and filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+
+
+def messageRecived():
+    print('message was received!!!')
 
 
 @app.route('/')
@@ -55,6 +60,18 @@ def download_file():
     return send_file(path, as_attachment=True)
 
 
+@app.route('/devchat')
+def grpchat():
+    return render_template('devChat.html')
+
+
+@socketio.on('my event')
+def handle_my_custom_event(json):
+    #print('recived my event: ' + str(json))
+    socketio.emit('my response', json, callback=messageRecived)
+
+
 if __name__ == '__main__':
+    socketio.run(app)
     # app.run(debug=True)
-    app.run()
+    # app.run()
