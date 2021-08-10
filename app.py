cc912a4ab869e2c06a84cd2abcfeb8b167bb2de4
simran1199 <simran3579singh@gmail.com>
2021-08-10 20:01:32 +0530

    added includes and done refactoring

--- app.py
+++ app.py
@@ -116,6 +116,6 @@ def handle_my_custom_event(json):
 
 
 if __name__ == '__main__':
-    socketio.run(app)
-    # app.run(debug=True)
+    # socketio.run(app)
+    socketio.run(app, debug=True)
     # app.run()
